9c52f0201247e59e4aab0783091ae900c4123117
Konstantin Belousov <kombuchamp@gmail.com>
2019-06-27 02:28:14 +0300

    Implement builder

---
+++ Builder/Builder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    /// <summary>
+    /// Abstract builder class.
+    /// Provides interface to construct complex object part by part.
+    /// </summary>
+    public abstract class Builder
+    {
+        protected Product _product;
+        public Builder()
+        {
+            _product = new Product();
+        }
+        public abstract void BuldPartA();
+        public abstract void BuldPartB();
+        public abstract void BuldPartC();
+        public Product Result => _product;
+    }
+}
---
+++ Builder/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    /// <summary>
+    /// "Complex" object which is built using builder
+    /// </summary>
+    public class Product
+    {
+        List<string> _content = new List<string>();
+
+        public IList<string> Content => _content;
+        public void Add(string s)
+        {
+            _content.Add(s);
+        }
+    }
+}
