122232b542c0c5f992b658b84e60a0ec22054659
Konstantin Belousov <kombuchamp@gmail.com>
2019-07-13 17:50:25 +0300

    Add product classes

---
+++ FactoryMethod/ConcreteProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    public class ConcreteProduct: Product
+    {
+        public ConcreteProduct()
+        {
+            Console.WriteLine("Concrete product created");
+        }
+    }
+}
---
+++ FactoryMethod/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    public abstract class Product
+    {
+    }
+}
