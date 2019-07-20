538d3f3a3636e6feeacc144877eee51dfcc6f054
Konstantin Belousov <kombuchamp@gmail.com>
2019-07-20 03:42:04 +0300

    Add thread safe version

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Singleton
 {
@@ -8,7 +10,21 @@ namespace Singleton
         {
             var instance1 = Singleton.Instance;
             var instance2 = Singleton.Instance;
-            Console.WriteLine(ReferenceEquals(instance1, instance2));
+            Console.WriteLine($"instance1 == instance2 ? {ReferenceEquals(instance1, instance2)}\n");
+
+            int taskCount = 10;
+            var tasks = new Task<ThreadSafeSingleton>[taskCount];
+            Console.WriteLine($"Running {taskCount} Tasks acquiring ThreadSafeSingleton instance on {Environment.ProcessorCount} cores...\n");
+            for (int i = 0; i < taskCount; i++)
+            {
+                tasks[i] = Task.Run(() => ThreadSafeSingleton.Instance);
+            }
+            Task.WaitAll(tasks);
+            Console.WriteLine("\nDone");
+            foreach (var task in tasks)
+            {
+                Console.WriteLine($"Task #{task.Id}, instance wtih hash code: {task.Result.GetHashCode()}");
+            }
         }
     }
 }
---
+++ Singleton/ThreadSafeSingleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class ThreadSafeSingleton
+    {
+        private static ThreadSafeSingleton _instance;
+        private static object syncRoot = new object();
+        public static ThreadSafeSingleton Instance
+        {
+            get
+            {
+                // Locking is expensive operation, so its better to check twice than lock everytime
+                if (_instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        Console.WriteLine("** Inside lock section **");
+                        if (_instance == null)
+                        {
+                            _instance = new ThreadSafeSingleton();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public string Name { get; }
+
+        private ThreadSafeSingleton()
+        {
+            Name = "ThreadSafeSingleton";
+        }
+    }
+}
